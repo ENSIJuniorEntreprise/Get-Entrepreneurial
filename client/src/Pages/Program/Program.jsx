e31123656b8cdafc533ca2d53e2e68a65b0e23cf
selimketata <121767998+selimketata@users.noreply.github.com>
2023-12-02 21:38:35 +0100

    Program Page

--- client/src/Pages/Program/Program.jsx
+++ client/src/Pages/Program/Program.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import './Program.css'
+import Timeline from "./Timeline";
 
 export default function Program() {
   return (
     <div >
-        program
+        <Timeline></Timeline>
     </div>
   );
 }
