2043c7744a79b90037c4ee0365aba6dae929ab61
selimketata <121767998+selimketata@users.noreply.github.com>
2024-01-19 02:54:42 +0100

    env

--- backend/Server.js
+++ backend/Server.js
@@ -1,40 +1,33 @@
-const express = require("express");
-
-const app = express()
-
-const morgan = require('morgan')
-
-const cors = require('cors')
+require('dotenv').config(); // Load environment variables from .env file
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
 const path = require('path');
-const mongoose = require("mongoose")
+const mongoose = require('mongoose');
 
 const port = process.env.PORT || 8000;
+const mongoURI = process.env.MONGO_URI;
 
-const mongoURI = "mongodb+srv://ketata15selim:projetevent@cluster0.gbvzliv.mongodb.net/test"
+app.use(express.json());
+app.use(cors());
+app.use(morgan('tiny'));
 
-app.use(express.json())
+app.get('/', (req, res) => {
+    res.send('hello hello');
+});
 
-app.use(cors())
-
-app.use(morgan('tiny'))
-
-app.get('/',(req,res)=>{
-    res.send("hello hello")
-})
-
-app.use("/inscription",require(path.join(__dirname,"./Routes/inscription.js")))
-app.use("/article",require(path.join(__dirname,"./Routes/article.js")))
-app.use("/collab",require(path.join(__dirname,"./Routes/collab.js")))
-
-
+app.use('/inscription', require(path.join(__dirname, './Routes/inscription.js')));
+app.use('/article', require(path.join(__dirname, './Routes/article.js')));
+app.use('/collab', require(path.join(__dirname, './Routes/collab.js')));
 
 mongoose.connect(mongoURI, {
-  serverSelectionTimeoutMS: 5000,
-}).then((result) => {
-  console.log("connected to database");
-  app.listen(port, () => {
-      console.log(`running on port ${port}`);
-  });
-}).catch(err => {
-  console.error("Error connecting to the database:", err);
+    serverSelectionTimeoutMS: 5000,
+}).then(() => {
+    console.log('Connected to the database');
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+}).catch((err) => {
+    console.error('Error connecting to the database:', err);
 });
