ff3f8b1a16607a79949001a6a78733cc5e2fb1f5
selimketata <121767998+selimketata@users.noreply.github.com>
2024-01-21 08:22:10 +0100

    backend

--- backend/Server.js
+++ backend/Server.js
@@ -1,4 +1,6 @@
-require('dotenv').config(); // Load environment variables from .env file
+const https = require('https');
+const fs = require('fs');
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
@@ -8,6 +10,11 @@ const mongoose = require('mongoose');
 
 const port = process.env.PORT || 8000;
 const mongoURI = process.env.MONGO_URI;
+
+const httpsOptions = {
+    key: fs.readFileSync('./cert/key.pem'),
+    cert: fs.readFileSync('./cert/cert.pem'),
+};
 
 app.use(express.json());
 app.use(cors());
@@ -21,13 +28,22 @@ app.use('/api/inscription', require(path.join(__dirname, './Routes/inscription.j
 app.use('/api/article', require(path.join(__dirname, './Routes/article.js')));
 app.use('/api/collab', require(path.join(__dirname, './Routes/collab.js')));
 
+// Connect to MongoDB
 mongoose.connect(mongoURI, {
     serverSelectionTimeoutMS: 5000,
-}).then(() => {
+})
+.then(() => {
     console.log('Connected to the database');
-    app.listen(port, () => {
+    // Start HTTPS server
+    https.createServer(httpsOptions, app).listen(port, () => {
         console.log(`Server is running on port ${port}`);
     });
-}).catch((err) => {
+})
+.catch((err) => {
     console.error('Error connecting to the database:', err);
+});
+
+// Error handling for HTTPS server
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', reason.stack || reason);
 });
